421b213cb852324908c4d601749714bac4975831
daphnegru <daphne.gruber@gmail.com>
2023-07-11 18:04:39 +0300

    intervals

--- src/main/java/com/weather/service/WeatherService.java
+++ src/main/java/com/weather/service/WeatherService.java
@@ -16,17 +16,40 @@ public class WeatherService {
 
     public String getWeatherConditions(String location, String rule, String operator) throws IOException, InterruptedException, JSONException {
         JSONObject newApi;
+        JSONObject response = new JSONObject();
         try {
             newApi = new JSONObject(weatherApiBuilder.getApi(location));
             String[] rules = rule.split(",");
             JSONArray intervals = newApi.getJSONObject("data").getJSONArray("timelines").getJSONObject(0).getJSONArray("intervals");
-            for (int i = 0; i < intervals.length(); i++) {
-                JSONObject values = intervals.getJSONObject(i).getJSONObject("values");
-                values.put("condition_met", conditionChecker.checkIfConditionMet(values, rules, operator));
-            }
+            response.put("timelines", getTimelines(intervals, rules, operator));
         } catch (IOException | JSONException e) {
             throw new RuntimeException(e);
         }
-        return newApi.toString();
+        return response.toString();
+    }
+
+    private JSONArray getTimelines(JSONArray intervals, String[] rules, String operator) throws JSONException {
+        JSONArray timelines = new JSONArray();
+        boolean firstMet = conditionChecker.checkIfConditionMet(intervals.getJSONObject(0).getJSONObject("values"), rules, operator);
+        String startTime = intervals.getJSONObject(0).getString("startTime");
+        for (int i = 1; i < intervals.length(); i++) {
+            JSONObject values = intervals.getJSONObject(i).getJSONObject("values");
+            JSONObject curr = new JSONObject();
+            boolean isMet = conditionChecker.checkIfConditionMet(values, rules, operator);
+            if (isMet != firstMet) {
+                curr.put("startTime", startTime);
+                curr.put("endTime", intervals.getJSONObject(i).getString("startTime"));
+                curr.put("condition_met", firstMet);
+                timelines.put(curr);
+                startTime = intervals.getJSONObject(i).getString("startTime");
+                firstMet = isMet;
+            }
+        }
+        JSONObject curr = new JSONObject();
+        curr.put("startTime", startTime);
+        curr.put("endTime", intervals.getJSONObject(intervals.length()-1).getString("startTime"));
+        curr.put("condition_met", firstMet);
+        timelines.put(curr);
+        return timelines;
     }
 }
